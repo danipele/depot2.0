accb5ca70c8eee862b11187aa50f02448a8a707c
danipele <danipele@yahoo.com>
2020-12-22 19:41:43 +0200

    add react files for multi types of payment on fe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -86,4 +86,17 @@ class OrdersController < ApplicationController
 
     redirect_to store_index_url, notice: 'Your cart is empty'
   end
+
+  def pay_type_params
+    case order_params[:pay_type]
+    when 'Credit Card'
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    when 'Check'
+      params.require(:order).permit(:routing_number, :account_number)
+    when 'Purchase Order'
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
 end
