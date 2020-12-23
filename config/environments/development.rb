948156ed86ea37b8084d5c324567db31f01e2f23
danipele <danipele@yahoo.com>
2020-12-23 18:19:49 +0200

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,3 +55,17 @@ config.webpacker.check_yarn_integrity = true
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
+end
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   test 'check pay type fields' do
     visit store_index_url
     first('.catalog li').click_on 'Add to Cart'
@@ -31,5 +33,41 @@ class OrdersTest < ApplicationSystemTestCase
     assert_no_selector '#order_credit_card_number'
     assert_no_selector '#order_expiration_date'
     assert_no_selector '#order_po_number'
+  end
+
+  test 'check order' do
+    LineItem.delete_all
+    Order.delete_all
+
+    visit store_index_url
+    first('.catalog li').click_on 'Add to Cart'
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    select 'Check', from: 'pay_type'
+    fill_in 'Routing #', with: '123456'
+    fill_in 'Account #', with: '987654'
+
+    perform_enqueued_jobs do
+      click_button 'Place Order'
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+    assert_equal 'Dave Thomas', order.name
+    assert_equal '123 Main Street', order.address
+    assert_equal 'dave@example.com', order.email
+    assert_equal 'Check', order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['dave@example.com'], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
   end
 end
